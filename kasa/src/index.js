a7087ae248db031b5711963a7bc9837b84db813c
Mathis Chatelain <mathis.chatelain@socotec.com>
2022-12-01 15:01:31 +0100

    step 1.2 move css and static + rename

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+import Home from './pages';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
