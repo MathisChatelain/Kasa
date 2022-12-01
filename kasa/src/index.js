74400650b733a09ff82bb4bf30b3d39dfe967005
Mathis Chatelain <mathis.chatelain@socotec.com>
2022-12-01 15:45:07 +0100

    fix v18 root render

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './index.css';
 import Home from './pages';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+const rootElement = createRoot(document.getElementById("root"));
+rootElement.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
